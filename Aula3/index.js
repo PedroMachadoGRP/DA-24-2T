6896cfc0aecee08d070443a8d23cd142287981b9
GabrielGaribaldi <maria.eduarda.paz.ruas@gmail.com>
2024-08-12 17:23:15 -0300

    Foi adicionada a pasta Aulas3

---
+++ Aula3/index.js
@@ -0,0 +1,42 @@
+const primeiroNome = "Pedro"
+const sobrenome = "Machado"
+const idade = 18
+const estudante = "Sou estudante"
+
+console.log("Ola meu nome é",primeiroNome,"Meu sobrenome é", sobrenome, "eu tenho",idade, "anos e",estudante );
+
+const got = "Game of Thrones";
+const temporadasDeGot = "8";
+console.log(typeof got);
+console.log(typeof temporadasDeGot);
+
+let novaVariavel
+console.log(typeof novaVariavel);
+novaVariavel = 2
+console.log(typeof novaVariavel);
+novaVariavel = undefined
+console.log(typeof  novaVariavel);
+
+/*let pergunta1 = prompt("Insira o seu nome")
+console.log(typeof pergunta1);
+let pergunta2 = prompt("Insira sua idade")
+pergunta2 = Number()
+console.log(typeof pergunta2);*/
+
+const idadeNumero = 23
+const idadeTexto = idadeNumero.toString()
+console.log(idadeNumero);
+console.log(typeof idadeNumero);
+console.log(idadeTexto);
+console.log(typeof idadeTexto);
+
+const idadeTexto2 = "23"
+const idadeNumero2 = Number(idadeTexto2)
+console.log(typeof idadeNumero2);
+console.log(typeof idadeTexto2);
+
+
+
+
+
+
