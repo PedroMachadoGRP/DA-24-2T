dd720dea62f4f1d12d98fcd1b7860dbf8b653d1d
r4di0head <bocchiannajulia@gmail.com>
2024-08-15 17:19:23 -0300

    Adicionada a pasta Aula5

---
+++ Aula5/index.js
@@ -0,0 +1,65 @@
+/*
+1)
+let a1 = 10 // Variavel a inserindo um número
+let b1 = 10 // Variavel b inserindo um número
+
+console.log(b); // impressão do console
+
+b = 5 // equivalencia da variavel b sendo trocado por outro número
+console.log(a,b);
+
+//A função do código é transformar a variavel b de 10 para 5 e imprimir no console
+*/
+
+/*
+2)
+let a = 10 // variavel a inserindo o número 10
+let b = 20 // variavel b inserindo o número 20
+c = a // foi definido a opção c sendo equivalente a variavel a
+b = c // equivalencia da variavel b sendo trocada pela equivalencia da opção c
+a = b // equivalencia da variavel a sendo trocada pela equivalencia da variavel b 
+
+console.log(a,b,c); //console imprimindo as variaveis utilizadas
+*/
+
+//A função desse código é transferir o valor de uma variavel para a outra assim fazendo com que todas tenham o mesmo resultado
+
+/* 3)
+let p = prompt("Quantas horas você trabalha por dia?") //Variavel recebe a informação de quantas horas são trabalhadas
+//Um nome melhor para a variavel seria horasTrabalhadas
+let t = prompt("Quanto você recebe por dia?") //A variavel recebe a informação de quanto é recebido por hora
+//Um nome melhor para a variavel seria dinherioPorHora
+console.log("Voce recebe",t, "/",p,"por hora"); // o console imprime as variaveis anteriores */
+
+/*
+let nomeVazio //Variavel vazia
+let idadeVazio //Variavel vazia
+
+console.log(typeof nomeVazio,typeof idadeVazio); // As variaveis se equivalem a undefined,pois nenhum valor foi definido para elas
+
+let nome = prompt("Qual o seu nome?") //Solicita ao usuario o nome
+let idade = prompt("Qual a sua idade") //Solicidta ao usuario a idade
+console.log("Olá",(nome),"você tem",(idade),"anos"); // imprime o nome e idade do usuario no console
+alert("Olá, " + nome + " você tem "+ idade + " anos.") //Imprime o nome e a idade na tela
+
+*/
+/*
+let pergunta1 = ("Você 20 reais?")
+let pergunta2 = ("Você está pronto pra prova?")
+let pergunta3 = ("Você quebrou o braço?")
+let resposta1 = " -sim"
+let resposta2 = " -não"
+let resposta3 = " -não"
+alert(pergunta1 + resposta1)
+alert(pergunta2 + resposta2)
+alert(pergunta3 + resposta3)
+*/
+
+let a = 10
+let b = 25
+c = a
+a = b
+b = c
+console.log("O novo valor de a é", a);
+console.log("O novo valor de b é", b);
+
