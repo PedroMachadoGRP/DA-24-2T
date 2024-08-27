5dccee1eb6211c05cf3aad20952d29ea1eff0350
r4di0head <bocchiannajulia@gmail.com>
2024-08-27 17:20:38 -0300

    Foi adicionado a pasta Aula 10

---
+++ Aula10/index.js
@@ -0,0 +1,175 @@
+/*
+let condicao1 = true
+if(condicao1){
+    //acao
+    console.log("exibiu condicao1");
+    
+}
+let condicao2 = false
+if(condicao2){
+    //acao
+    console.log("exibiu condicao2");
+}
+*/
+/*
+let num1 = prompt("Insira um número")
+let num2 = prompt("Insira um número igula ao primeiro")
+if (num1 === num2){
+    console.log("Ambos os número são iguais");
+    
+}
+*/
+/*
+let condicao1 = true
+if(condicao1){
+    console.log('a condição é verdadeira');
+}else{
+    console.log('a condição é falsa');
+    
+}
+*/
+/*
+let num1 = Number(prompt("Insira um número"))
+let num2 = Number(prompt("Insira um número igual ao primeiro"))
+if(num1 === num2){
+    console.log("Ambos os números são iguais");
+}else{
+    console.log("Os números são diferentes");
+    
+}
+*/
+/*
+let condicao1 = false
+let condicao2 = false
+if (condicao1) {
+    console.log("condicao1 verdadeiro");
+}else if(condicao2){
+        console.log("Condicao2 é verdadeira");
+        
+    }else{
+        console.log("Entrou no else");
+        
+    }
+*/
+/*
+let num1 = Number(prompt("Insira um número"))
+let num2 = Number(prompt("Insira outro número"))
+if(num1 === num2){
+    console.log("Ambos os números são iguais");
+}else if(num1 > num2){
+    console.log("O primeiro número é maior que o segundo");
+}else{
+    console.log("O primeiro número é menor que o segundo");
+    
+}
+*/
+/*
+let paisDeOrigem = prompt("Digite o seu pais de origem")
+switch(paisDeOrigem){
+    case 'Brasil' :
+        console.log("Brasileiro");
+        break
+    case 'Eua':
+        console.log("Norte americano");
+        break
+    case 'Inglaterra':
+        console.log("Inglês");
+        break
+    default:
+        console.log("Nacionalidade não encontrada");
+        break
+        
+}
+*/
+/*
+let pokemonInicial = prompt("Digite o seu pokemon incial")
+switch(pokemonInicial){
+    case "Charmander":
+        console.log("Fogo");
+        break
+    case "Squirtle":
+        console.log("água");
+        break
+    case "Bulbasaur":
+        console.log("Planta e veneno");
+        break
+    case "Cyndaquil":
+        console.log("Fogo");
+        break
+    case "Chikorita":
+        console.log("Planta");
+        break
+    case "Totodile":
+        console.log("água");
+        break
+    case "Torchic":
+        console.log("Fogo");
+        break
+    case "Mudkip":
+        console.log("Água");
+        break
+    case "Trickle":
+        console.log("Planta");
+        break
+    default:
+        console.log("Esse item não esta na lista");
+        break
+        
+}
+*/
+/*
+let personagensHarry = prompt("Insira um personagem")
+switch(personagensHarry){
+    case "Hermione":
+        console.log("Grifinória");
+        break
+    case "Ana":
+        console.log("Lufa-lufa");
+        break
+    case "Luna":
+        console.log("Corvinal");
+        break
+    case "Narcisa":
+        console.log("Sonserina");
+        break
+    default:
+        console.log("Personagem invalido");
+        break
+}
+*/
+/*
+let condicao1 = true
+let condicao2 = false
+if(condicao1 && condicao2){
+    console.log("As duas condições sao verdadeira");
+}else{
+    console.log("Alguma condição é verdadira");
+    
+}
+if(condicao1 || condicao2){
+    console.log("Alguma das condições é verdadeira");
+}else{
+    console.log("Todas são falsas");
+    
+}
+if(!condicao1){
+    console.log("A condicao1 é falsa");
+}else{
+    console.log("A condicao1 é verdadira");
+    
+}
+*/
+/*
+let idade = Number(prompt("Qual a sua idade").toLocaleLowerCase())
+let escola = prompt("Você terminou o ensino médio")
+let faculdade = prompt("Você está cursando outra faculdade")
+if(idade < 18){
+    console.log("Você não pode cursar a faculdade pois é menor de idade");
+}else if(escola === "não"){
+    console.log("Você não pode cursar a faculdade pois não terminou o ensino médio");
+}else if (faculdade === "sim"){
+    console.log("Você não pode cursar a faculdade pois está cursando outro curso");
+}else{
+    console.log("Você pode entrar em uma faculdade");
+}
+*/
