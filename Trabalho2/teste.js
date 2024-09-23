7105bdbe8893bb54e0f9807199bba40870258d03
r4di0head <bocchiannajulia@gmail.com>
2024-09-23 14:16:58 -0300

    Atualizado trabalho 2

--- Trabalho2/teste.js
+++ Trabalho2/teste.js
@@ -1,32 +1,32 @@
-const comecar = true
-const personagens = [
-    {nome : "Place",
-    classe : "any",
-    nivel : "100",
-    hp : "999",
-    habilidade : ["Invensibilidade","Poder infinito"]},
-    {nome : "andy",
-        classe : "any",
-        nivel : "100",
-        hp : "999",
-        habilidade : ["Invensibilidade","Poder infinito"]}
-]
 
-let qualPersonagem = prompt("Qual personagem você quer escolher para fazer uma ação ")
 
-const personagemId = qualPersonagem
-const personagemEncontradao = personagens.find(personagen => personagen.nome === personagemId)
-if(personagemEncontradao){
-    console.log("Personagem encontrado com sucesso");
-}    let perguntaHabilidade = prompt("Qual habilidade você quer usar?")
-const habilidadeId = perguntaHabilidade
-const habilidadeEncontrado = personagens.find(habilidade => habilidade.habilidade === habilidadeId)
-if(habilidadeEncontrado){
-    console.log("Você usou a habilidade", perguntaHabilidade);
-    
-}else {
-    console.log("Algo deu errado");
-    
-} 
 
-console.log(personagens);
+
+
+function contaATe10 () {
+      let soma = 11
+
+    for(let contador = 10; contador >= 0; contador-- ) {
+        soma --
+       
+
+        if(contador % 2 == 0) {
+            console.log("Tic ")
+        }
+        else if(contador % 2 !== 0) {
+            console.log("Tac")
+        }
+        
+        if(soma === 0) {
+            console.log("BOMMMMMM")
+        }
+    }
+
+ 
+
+
+
+}
+
+
+contaATe10()
