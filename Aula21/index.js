2cc2876eacb6c1be2c858bd70b1915bcd54599da
Matheus da silva <matheussilvainter1234@gmail.com>
2024-09-30 14:56:28 -0300

    Foi adicionado a Aula 21

---
+++ Aula21/index.js
@@ -0,0 +1,91 @@
+/*
+let matriz = [
+    [2,4,6],
+    [8,10,12],
+    [14,16,18]
+]
+console.log(matriz[2][1]);
+//Será imprimido no console : 16, porque ele imprimira a terceira linha e segunda coluna da matriz
+*/
+/*
+let  matriz = [
+    [1, 2],
+    [3, 4],
+    [5, 6, 7]
+]
+for(let i = 0; i<matriz.length; i++){
+    for(let j = 0 ; j< matriz[i].length; j++){
+        if(matriz[i][j] %2 === 0 ){
+            console.log(matriz[i][j]);
+            
+        }
+    }
+}
+    */
+//Serão imprimidos os números 2,4,6. Eles serão imprimido porque a condicional verifica se o número é par ou impar
+/*
+let matriz = [
+    [5,10,15],
+    [20,25,30],
+    [35,40,45]
+]
+let soma = 0
+for(let i = 0; i<matriz.length; i++){
+    for(let j = 0 ; j< matriz[i].length; j++){
+        soma += matriz[i][j]
+    }
+}
+console.log(soma);
+//O valor final da verificação da soma será = 225. O valor foi calculado com um loop que soma todos os itens da matriz
+*/
+
+/*
+let mapaJogo= [
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,1,1,1,0],
+    [2,2,2,0,0],
+    [2,2,2,0,0]
+]
+console.log(mapaJogo);
+*/
+/*
+let pontuação = [
+    [1,2,3,4],
+    [5,5,5,5],
+    [3,3,3,3]
+]
+let soma1 = 0
+    for(let j = 0 ; j < pontuação[0].length; j++){
+    soma1 += pontuação[0][j]
+    }
+
+console.log(`A pontuação do jogador 1 foi ${soma1}`);
+
+let soma2 = 0
+    for(let j = 0 ; j< pontuação[1].length; j++){
+    soma2 += pontuação[1][j]
+    }
+
+console.log(`A pontuação do jogador 2 foi ${soma2}`);
+
+let soma3 = 0
+    for(let j = 0 ; j< pontuação[2].length; j++){
+    soma3 += pontuação[2][j]
+    }
+
+console.log(`A pontuação do jogador 3 foi ${soma3}`);
+*/
+/*
+let matriz = [
+    [1,2,3,4],
+    [10,20,30,40],
+    [3,6,9,12],
+    [5,10,15,20]
+]
+matriz[0].reverse()
+matriz[1].reverse()
+matriz[2].reverse()
+matriz[3].reverse()
+console.log(matriz);
+*/
